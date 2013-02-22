02c32caa67e6f265bc8a85ff3168a80321a83f93
Alex Clark <aclark@aclark.net>
2013-02-21 21:17:26 -0500

    WTF

--- publishconf.py
+++ publishconf.py
@@ -11,15 +11,18 @@ DISQUS_SITENAME = 'aclark-blog'
 FEED_ATOM = 'feeds/all.atom.xml'
 GITHUB_URL = 'https://github.com/ACLARKNET/blog'
 GOOGLE_ANALYTICS = 'UA-34988446-1'
+LINKS = (
+    ('ACLARK.NET, LLC', 'http://aclark.net'),
+)
 SITENAME = u'Alex Clark - Python Web Developer'
 SITEURL = 'http://aclark.net/blog'
 SOCIAL = (
-    ('aclark4life', 'http://twitter.com/aclark4life'),
-    ('aclark4life (Gittip)', 'https://www.gittip.com/aclark4life'),
     ('atom feed (Django)', 'http://blog.aclark.net/feeds/Django.atom.xml'),
     ('atom feed (Mozilla)', 'http://blog.aclark.net/feeds/Mozilla.atom.xml'),
     ('atom feed (Plone)', 'http://blog.aclark.net/feeds/Plone.atom.xml'),
     ('atom feed (Python)', 'http://blog.aclark.net/feeds/Python.atom.xml'),
+    ('aclark4life', 'http://twitter.com/aclark4life'),
+    ('aclark4life (Gittip)', 'https://www.gittip.com/aclark4life'),
 )
 TAG_FEED_ATOM = 'feeds/%s.atom.xml'
 TAG_FEED_RSS = None
